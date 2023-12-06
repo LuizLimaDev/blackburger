e6c8c6ef319763be2635e32d1b875f2da3ce55a6
luizlimadev <devluizlima@gmail.com>
2023-12-06 15:40:09 -0300

    feat: Products details static page

--- src/types/products.ts
+++ src/types/products.ts
@@ -1,3 +1,5 @@
+import { Dispatch, SetStateAction } from "react";
+
 export type IProduct = {
   id: number;
   img: string;
@@ -6,4 +8,10 @@ export type IProduct = {
   price: number;
   category_id: number;
   favorite?: boolean;
+};
+
+export type IProducts = {
+  products: IProduct[];
+  currentProductId: number;
+  setCurrentProductId: Dispatch<SetStateAction<number>>;
 };
