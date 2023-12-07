35d0a286e529c1f07e8f93a6340b297bc2e6bde9
luizlimadev <devluizlima@gmail.com>
2023-12-06 22:27:19 -0300

    feat: Modal Delivery static page

--- src/app/(home-routes)/cart/(..)modaldelivery/page.tsx
+++ src/app/(home-routes)/cart/(..)modaldelivery/page.tsx
@@ -1,14 +1,18 @@
+import DeliveryForm from "@/components/Forms/DeliveryForm/DeliveryForm";
 import Link from "next/link";
-import DeliveryForm from "../../../../components/Forms/DeliveryForm/DeliveryForm";
 
 export default function ModalDelivery() {
   return (
     <>
       <div className="modal-shade z-50">
-        <div className="flex w-[320px] flex-col items-center rounded-lg bg-gray-300 drop-shadow-bb-4">
+        <div
+          className="
+            flex-col-center w-[320px] rounded-lg bg-gray-bb-300 drop-shadow-bb-4
+          "
+        >
           <Link
             href="/cart"
-            className="mr-4 mt-4 self-end font-lilita text-base text-gray-400"
+            className="mr-4 mt-4 self-end font-lilita text-base text-gray-bb-400"
           >
             X
           </Link>
