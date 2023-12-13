8d25f6e5e8901179bd6607eafafcfe95c04d6867
luizlimadev <devluizlima@gmail.com>
2023-12-13 15:15:33 -0300

    fix: Invalid GET export Response

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 const pool = require("../../../services/db/postgres");
 
-export async function GET(response: Response) {
+export async function GET() {
   const res = await pool.query("select * from products_categories");
   const data = res.rows;
 
