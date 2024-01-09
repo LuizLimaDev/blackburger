ac77c15c4ad14e4354e33cf08118051fd1742577
luizlimadev <devluizlima@gmail.com>
2024-01-09 16:53:04 -0300

    refactor: Moving mocks to your repective module folder

---
+++ src/context/__mocks__/contextValues.ts
@@ -0,0 +1,19 @@
+const contextValues = {
+  productsOnCart: [],
+  setProductsOnCart: () => {},
+  currentProductId: 0,
+  setCurrentProductId: () => {},
+  setQuantityCounter: () => {},
+  decreaseProductCounter: () => {},
+  increaseProductCounter: () => {},
+  addProductToCart: () => {},
+  removeProductFromCart: () => {},
+  cartSubtotal: 0,
+  cartDiscount: 0,
+  tax: 0,
+  cartTotalPrice: 0,
+  decreaseProductQuantity: () => {},
+  increaseProductQuantity: () => {},
+};
+
+export default contextValues;
