2f350ee207f898d3102dddc4e1838d0d633051ec
luizlimadev <devluizlima@gmail.com>
2024-01-11 17:06:27 -0300

    chore: Category card tests

--- __tests__/components/Surfaces/CategoryCard.test.tsx
+++ __tests__/components/Surfaces/CategoryCard.test.tsx
@@ -1,4 +1,5 @@
 import CategoryCard from "@/components/Surfaces/CategoryCard/CategoryCard";
+import priceConvert from "@/utils/priceConvert";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 
@@ -16,5 +17,36 @@ describe("Render category card", () => {
     render(<CategoryCard product={productMock} />);
     const image = screen.getByAltText(productMock.name);
     expect(image).toBeInTheDocument();
+  });
+
+  test("render product name", () => {
+    render(<CategoryCard product={productMock} />);
+    const productName = screen.getByRole("heading");
+    expect(productName).toBeInTheDocument();
+    expect(productName).toHaveClass("bb-title");
+  });
+
+  test("render product description", () => {
+    render(<CategoryCard product={productMock} />);
+    const productDescription = screen.getByText(productMock.description);
+    expect(productDescription).toBeInTheDocument();
+  });
+
+  test("render add to cart icon", () => {
+    render(<CategoryCard product={productMock} />);
+    const addToCart = screen.getByAltText("adicionar ao carrinho");
+    expect(addToCart).toBeInTheDocument();
+  });
+
+  test("render favorite icon", () => {
+    render(<CategoryCard product={productMock} />);
+    const notFavoriteIcon = screen.getByAltText("item não favoritado");
+    expect(notFavoriteIcon).toBeInTheDocument();
+  });
+
+  test("render product price", () => {
+    render(<CategoryCard product={productMock} />);
+    const price = screen.getByText(priceConvert(productMock.price));
+    expect(price).toBeInTheDocument();
   });
 });
