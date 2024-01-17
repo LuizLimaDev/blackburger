de188c09920c90906bcca15e295b2ed6ce805833
luizlimadev <devluizlima@gmail.com>
2024-01-17 18:01:51 -0300

    refactor: Home

--- src/utils/filterProductsById.ts
+++ src/utils/filterProductsById.ts
@@ -1,7 +1,7 @@
 import { TProduct } from "@/types/products";
 
-function filterProductsById(products: TProduct[], condition: number) {
+const filterProductsById = (products: TProduct[], condition: number) => {
   return products.filter((product) => product.id % 2 === condition);
-}
+};
 
 export default filterProductsById;
